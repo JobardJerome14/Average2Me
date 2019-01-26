d898215a404f71dd55051209fb941862e32305e7
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2019-01-26 09:52:36 +0100

    [ADD] bind pieChart to answer activtiy

--- app/src/main/java/averageme/first/com/average2me/ui/AnswerActivity.java
+++ app/src/main/java/averageme/first/com/average2me/ui/AnswerActivity.java
@@ -1,11 +1,25 @@
 package averageme.first.com.average2me.ui;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import averageme.first.com.average2me.R;
 import averageme.first.com.average2me.api.SharedP;
 import averageme.first.com.average2me.models.Ask;
@@ -15,6 +29,7 @@ public class AnswerActivity extends ActivityBase {
 
     private Ask ask;
     private String response;
+    private PieChart pieChart;
 
     SharedP sharedP;
     TextView a;
@@ -44,6 +59,7 @@ public class AnswerActivity extends ActivityBase {
     }
 
     private void bindView() {
+
         this.ask_label = findViewById(R.id.ask);
         this.ask_label.setText(ask.getAsk());
 
@@ -64,6 +80,43 @@ public class AnswerActivity extends ActivityBase {
 
         this.vb = findViewById(R.id.vb);
         this.vb.setText(String.valueOf(valeur_b));
+
+        this.pieChart = findViewById(R.id.pieChart);
+        List<PieEntry> entries = new ArrayList<>();
+
+
+        entries.add(new PieEntry(35.0f, "Green"));
+        entries.add(new PieEntry(65.0f, "Red"));
+
+        PieDataSet set = new PieDataSet(entries, "Pie Results");
+
+        // add a lot of colors
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+/*      for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);*/
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+/*        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);*/
+
+/*        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);*/
+
+/*
+        colors.add(ColorTemplate.getHoloBlue());
+*/
+
+        set.setColors(colors);
+
+        PieData data = new PieData(set);
+        pieChart.setData(data);
+        pieChart.invalidate(); // refresh
+
 
         this.btn_next = findViewById(R.id.btn_next);
         btn_next.setOnClickListener(new View.OnClickListener() {
