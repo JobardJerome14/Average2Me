9272d86263f937f6a9a22ba809134bab26889b09
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2019-01-27 19:23:02 +0100

    [EVOL] add some sharePref

--- app/src/main/java/averageme/first/com/average2me/api/SharedP.java
+++ app/src/main/java/averageme/first/com/average2me/api/SharedP.java
@@ -5,7 +5,10 @@ import android.content.SharedPreferences;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+
 import averageme.first.com.average2me.models.Ask;
+import averageme.first.com.average2me.models.AskList;
 
 public class SharedP {
     private Context context;
@@ -43,6 +46,23 @@ public class SharedP {
                 .apply();
     }
 
+
+    public AskList getCurrentAskList() {
+        Gson gson = new Gson();
+        String json = getSharedPref().getString(UserPrefHelper.USER_CURRENT_ASK_LIST, "");
+        return gson.fromJson(json, AskList.class);
+    }
+
+
+    public void setCurrentAskList(AskList askList) {
+        Gson gson = new Gson();
+        String json = gson.toJson(askList);
+
+        getSharedPref().edit()
+                .putString(UserPrefHelper.USER_CURRENT_ASK_LIST, json)
+                .apply();
+    }
+
     public String getCategoryId() {
         return getSharedPref().getString(UserPrefHelper.USER_CATEGORY_ID, "");
     }
@@ -54,11 +74,23 @@ public class SharedP {
     }
 
 
+    public Integer getReloadApi() {
+        return getSharedPref().getInt(UserPrefHelper.USER_RELOAD_API, 0);
+    }
+
+    public void setReloadApi(Integer reload) {
+        getSharedPref().edit()
+                .putInt(UserPrefHelper.USER_RELOAD_API, reload)
+                .apply();
+    }
+
+
     private class UserPrefHelper {
         final static String USER_PREF_DICO = "user_pref";
         final static String USER_ID = "user_id";
         final static String USER_CURRENT_ASK = "user_ask";
-        final static String USER_MODE_API = "mode_api";
+        final static String USER_CURRENT_ASK_LIST = "user_ask_list";
         final static String USER_CATEGORY_ID = "category_id";
+        final static String USER_RELOAD_API = "reload_api";
     }
 }
