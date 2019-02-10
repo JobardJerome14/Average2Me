700da8efb1de7a5164b747640a6ffd76a38e5512
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2019-02-10 22:17:09 +0100

    Update AskActivity.java

--- app/src/main/java/averageme/first/com/average2me/ui/AskActivity.java
+++ app/src/main/java/averageme/first/com/average2me/ui/AskActivity.java
@@ -11,6 +11,8 @@ import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
+
+
 import averageme.first.com.average2me.R;
 import averageme.first.com.average2me.api.API;
 import averageme.first.com.average2me.api.SharedP;
@@ -24,7 +26,7 @@ import averageme.first.com.average2me.ui.reusable.ActivityBase;
 import averageme.first.com.average2me.ui.reusable.LogUtils;
 
 
-public class AskActivity extends ActivityBase {
+public class AskActivity extends ActivityBase /*implements RewardedVideoAdListener*/ {
 
     String categoryId;
 
@@ -33,6 +35,7 @@ public class AskActivity extends ActivityBase {
     private SharedP sharedP;
     private Integer id_ask = 0;
     private TextView ask_label;
+    TextView ask_category_label;
     private Button btn_a;
     private Button btn_b;
 
@@ -41,6 +44,8 @@ public class AskActivity extends ActivityBase {
     private InterstitialAd mInterstitialAd;
     private ProgressBar spinner;
 
+    //private RewardedVideoAd mAd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +53,55 @@ public class AskActivity extends ActivityBase {
 
         setContentView(R.layout.activity_ask);
 
+/*        mAd = MobileAds.getRewardedVideoAdInstance(this);
+        mAd.setRewardedVideoAdListener(this);
+        mAd.loadAd(getResources().getString(R.string.avg_video), new AdRequest.Builder().build());*/
+
         this.sharedP = new SharedP(this);
         this.user_id = sharedP.getUserId();
 
         bindView();
     }
+
+/*    @Override
+    public void onRewardedVideoAdLoaded() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+        goto_answer();
+    }
+
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+
+    }
+
+    @Override
+    public void onRewardedVideoCompleted() {
+
+    }*/
 
     private void bindView() {
         this.spinner = findViewById(R.id.progressBar1);
@@ -72,8 +121,60 @@ public class AskActivity extends ActivityBase {
                 .build());
 
 
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                goto_answer();
+            }
+        });
 
         this.ask_label = findViewById(R.id.ask_label);
+
+        this.ask_category_label = findViewById(R.id.ask_category_label);
+        String cat_id = this.sharedP.getCategoryId();
+        switch(cat_id) {
+            case "1":
+                cat_id = getResources().getString(R.string.sport_btn_label);
+                break;
+            case "2":
+                cat_id = getResources().getString(R.string.food_btn_label);
+                break;
+            case "3":
+                cat_id = getResources().getString(R.string.politique_btn_label);
+                break;
+            case "4":
+                cat_id = getResources().getString(R.string.loisir_btn_label);
+                break;
+            case "5":
+                cat_id = getResources().getString(R.string.society_btn_label);
+                break;
+            default:
+                cat_id = getResources().getString(R.string.medley_btn_label);
+                break;
+        }
+
+
+        this.ask_category_label.setText(String.valueOf(cat_id));
 
         this.btn_a = findViewById(R.id.btn_a);
         btn_a.setOnClickListener(new View.OnClickListener() {
@@ -142,37 +243,70 @@ public class AskActivity extends ActivityBase {
         askList.setAnswered(answered);
         this.sharedP.setCurrentAskList(askList);
 
-        if(answered == 4) { //TODO reload API after 4 answers
+        if(answered == 4) {
             //need to call api for next question
-            this.sharedP.setReloadApi(1);
+            set_shp_reload_api(1);
             load_interstitial();
+            //load_video();
         }
         api.updateAverageMeAsk(this.id_ask, response, new ResultatCallback<RetourUpdate>() {
             @Override
             public void onWaitingResult(RetourUpdate result) {
-                goto_answer();
+                is_going_to_answer();//goto_answer();
             }
         });
     }
 
+    private void is_going_to_answer() {
+        if(this.sharedP.getReloadApi() == 0) {
+            goto_answer();
+        }
+    }
+
+
+/*    private void load_video() {
+        Integer test = this.sharedP.getReloadApi();
+        Log.i("load_video", String.valueOf(test));
+
+        if( test == 1) {
+            if (mAd.isLoaded()) {
+                Log.i("load_video isLoaded", String.valueOf(test));
+                LogUtils.log("FBEvent", IFBEvent.LOAD_EVENT+ " " + IFBEvent.INTERSTITIAL_KEY + " ask_video_isLoaded");
+                new FBevent(this, IFBEvent.LOAD_EVENT, IFBEvent.INTERSTITIAL_KEY, "ask_video_isLoaded");
+                mAd.show();
+            } else {
+                Log.d("load_video", "not loaded");
+                LogUtils.log("FBEvent", IFBEvent.LOAD_EVENT+ " " + IFBEvent.INTERSTITIAL_KEY + " ask_video_notLoaded");
+                new FBevent(this, IFBEvent.LOAD_EVENT, IFBEvent.INTERSTITIAL_KEY, "ask_video_notLoaded");
+                goto_answer();
+            }
+        } else {
+            Log.d("load_video", "test not equal to 1");
+            goto_answer();
+        }
+    }*/
+
 
     private void load_interstitial() {
         Integer test = this.sharedP.getReloadApi();
-        Log.i("getReloadApi", String.valueOf(test));
+        Log.i("load_interstitial", String.valueOf(test));
 
         if( test == 1) {
             if (mInterstitialAd.isLoaded()) {
-                Log.i("getReloadApi isLoaded", String.valueOf(test));
+                Log.i("load isLoaded", String.valueOf(test));
                 LogUtils.log("FBEvent", IFBEvent.LOAD_EVENT+ " " + IFBEvent.INTERSTITIAL_KEY + " ask_interstitial_isLoaded");
                 new FBevent(this, IFBEvent.LOAD_EVENT, IFBEvent.INTERSTITIAL_KEY, "ask_interstitial_isLoaded");
                 mInterstitialAd.show();
+
             } else {
-                Log.d("getReloadApi", "not loaded");
+                Log.d("load_interstitial", "not loaded");
                 LogUtils.log("FBEvent", IFBEvent.LOAD_EVENT+ " " + IFBEvent.INTERSTITIAL_KEY + " ask_interstitial_notLoaded");
                 new FBevent(this, IFBEvent.LOAD_EVENT, IFBEvent.INTERSTITIAL_KEY, "ask_interstitial_notLoaded");
+                goto_answer();
             }
         } else {
-            Log.d("getReloadApi", "test not equal to 1");
+            Log.d("load_interstitial", "test not equal to 1");
+            goto_answer();
         }
     }
 
