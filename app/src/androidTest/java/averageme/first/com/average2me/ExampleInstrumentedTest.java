b4a13e02fd846b7176eb69024e0bf222cef9a993
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2019-02-11 22:04:23 +0100

    [REFACTOR] unit test class

--- app/src/androidTest/java/averageme/first/com/average2me/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/averageme/first/com/average2me/ExampleInstrumentedTest.java
@@ -1,20 +1,13 @@
 package averageme.first.com.average2me;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+/*@RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
@@ -23,4 +16,4 @@ public class ExampleInstrumentedTest {
 
         assertEquals("averageme.first.com.average2me", appContext.getPackageName());
     }
-}
+}*/
